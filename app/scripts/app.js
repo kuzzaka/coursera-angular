5b5e8c6995282d86cd375b4f65f2da29abb16865
kuzzaka <kuzzaka@gmail.com>
2016-11-24 22:30:35 +0500

    remove redundant comments add routes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,20 @@
 'use strict';
 
 angular.module('confusionApp', ['ngRoute'])
-
+    .config(function($routeProvider) {
+      $routeProvider
+          .when('/contactus', {//route for contactus page
+            templateUrl: 'contactus.html',
+            controller: 'ContactController'
+          })
+          .when('/menu', {//route for menu page
+            templateUrl: 'menu.html',
+            controller: 'MenuController'
+          })
+          .when('/menu/:id', {//route for dish details page
+            templateUrl: 'dishdetail.html',
+            controller: 'DishDetailController'
+          })
+          .otherwise('/contactus')
+    })
 ;
--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -82,17 +82,10 @@ angular.module('confusionApp')
         date: ''
       };
 
-      //Step 1: Create a JavaScript object to hold the comment from the form
-
       $scope.submitComment = function() {
 
-        //Step 2: This is how you record the date
-        // "The date property of your JavaScript object holding the comment" = new Date().toISOString();
         $scope.comment.date = new Date().toISOString();
-        // Step 3: Push your comment into the dish's comment array
         $scope.dish.comments.push($scope.comment);
-
-        //Step 4: reset your form to pristine
         $scope.commentForm.$setPristine();
 
         $scope.comment = {
